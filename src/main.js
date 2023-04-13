a9690ab72375324a7e33f955a76d61c212ed7677
Oleksandr Moskaliuk <olekspm@gmail.com>
2023-04-13 22:05:11 +0200

    Added JS and optimized categories

--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
-import { renderCategories } from './js/categories';
-import { changeTheme } from './js/theme-toggle';
+import { renderCategories, changeCategoryStyle } from './js/categories';
+import { activateThemeToggle } from './js/theme-toggle';
 
 const pageEl = document.querySelector('html');
+
+// Build Categories list
+renderCategories();
 
 // На цей слухач кліків можна вішати всі кліки, що відбуваються на сторінці.
 // Але не перевірено, чи коректно буде працювати на обох сторінках. Якщо ні - поправимо)
 pageEl.addEventListener('click', event => {
   //   activate theme-toggle
-  const isInput = event.target.classList.contains('theme-toggle-js');
-  if (isInput) {
-    const checkboxEl = event.target;
-    changeTheme(pageEl, checkboxEl);
-  } else console.log('don`t turget'); //delete else block
-});
+  activateThemeToggle(event, pageEl);
 
-// Build Categories list
-renderCategories();
+  // change categories styles
+  changeCategoryStyle(event);
+});
