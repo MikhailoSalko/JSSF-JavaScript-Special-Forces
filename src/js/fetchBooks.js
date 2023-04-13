cb99964f8156eb3b9069cb69be4344aea13b6454
Mikhailo Salko <slipknotmisha@gmail.com>
2023-04-13 12:02:11 +0300

    add all functions to the class

--- src/js/fetchBooks.js
+++ src/js/fetchBooks.js
@@ -3,13 +3,39 @@ import axios from 'axios';
 export class FetchBooks {
   #BASE_URL = 'https://books-backend.p.goit.global/books/';
   constructor() {
-    this.bookId = bookId;
-    this.category = category;
+    this.bookId = 0;
+    this.category = '';
   }
 
-  fetchBookId(bookId) {
+  fetchCategoryList() {
     return axios
-      .get(`${this.#BASE_URL}${bookId}`)
+      .get(`${this.#BASE_URL}/category-list`)
+      .then(response => {
+        response.json();
+      })
+      .catch(console.error);
+  }
+
+  fetchTopBooks() {
+    return axios
+      .get(`${this.#BASE_URL}/top-books`)
+      .then(response => {
+        response.json();
+      })
+      .catch(console.error);
+  }
+  fetchBookId() {
+    return axios
+      .get(`${this.#BASE_URL}${this.bookId}`)
+      .then(response => {
+        response.json();
+      })
+      .catch(console.error);
+  }
+
+  fetchCategoryOfBooks() {
+    return axios
+      .get(`${this.#BASE_URL}${this.category}`)
       .then(response => {
         response.json();
       })
