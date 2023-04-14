85914f6e8e1caa8d144a6f5c8b5b3b90942d2338
Mikhailo Salko <slipknotmisha@gmail.com>
2023-04-15 00:57:49 +0300

    change to build up page

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
-
 import { renderCategories, changeCategoryStyle } from './js/categories';
 import { activateThemeToggle } from './js/theme-toggle';
-import { changeTheme } from "./js/theme-toggle";
-import { currentPageSwitcher } from "./js/current-page-switcher";
+import { changeTheme } from './js/theme-toggle';
+import { currentPageSwitcher } from './js/current-page-switcher';
 import './js/support-ukraine';
 
-
-const pageEl = document.querySelector("html");
+const pageEl = document.querySelector('html');
 // На сторінці Shop List шукати за класом ".shop-link"
-const currentPage = document.querySelector(".nav__link");
+const currentPage = document.querySelector('.nav__link');
 
 currentPageSwitcher(currentPage);
 
@@ -17,17 +15,16 @@ renderCategories();
 
 // На цей слухач кліків можна вішати всі кліки, що відбуваються на сторінці.
 // Але не перевірено, чи коректно буде працювати на обох сторінках. Якщо ні - поправимо)
-pageEl.addEventListener("click", event => {
+pageEl.addEventListener('click', event => {
   //   activate theme-toggle
   activateThemeToggle(event, pageEl);
 
-  const isInput = event.target.classList.contains("theme-toggle-js");
+  const isInput = event.target.classList.contains('theme-toggle-js');
   if (isInput) {
     const checkboxEl = event.target;
     changeTheme(pageEl, checkboxEl);
-  } else console.log("don`t turget"); //delete else block
+  } else console.log('don`t turget'); //delete else block
 });
-  // change categories styles
-  changeCategoryStyle(event);
-});
-
+// change categories styles
+//   changeCategoryStyle(event);
+// });
