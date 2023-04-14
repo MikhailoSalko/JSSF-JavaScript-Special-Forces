8d3909f289a224899e27291d83ae060f144bebb7
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-04-14 13:14:53 +0300

    add images files, attemps to render images

--- src/js/support-ukraine.js
+++ src/js/support-ukraine.js
@@ -7,8 +7,13 @@ supportBtnEl.addEventListener('click', renderMoreFoundations);
 
 const markup = supportUkraineItems.map(({ img, title, url }, index) => {
     const number = (index + 1).toString().padStart(2, '0');
-    return `<li><a href="${url}"><p>${number}</p><img src="" alt="${title}"></a></li>`});
-//    console.log(markup);
+    // console.log(img);
+    
+    return `<li><a href="${url}" class="support__link" aria-label="${title}" target="_blank" rel="noopener norefferer nofollow">
+    <p>${number}</p><img class="support__img" src="./images/support-ukraine/${img}" alt="${title}"></img></a></li>`
+
+});
+   console.log(markup);
 
 let startIndex = 0;
 
@@ -17,7 +22,7 @@ renderMarkup(markup, startIndex, findEndIndex(startIndex));
 function renderMarkup (arr, itemsCountStart, itemsCountEnd) {
     const markupToRender = arr.slice(itemsCountStart, itemsCountEnd).join('');
     startIndex = findEndIndex(startIndex);
-
+    // supportListEl.innerHTML = markupToRender;
    supportListEl.insertAdjacentHTML('beforeend', markupToRender);
 };
 
