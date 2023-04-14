6249ce644bd39f10ae5ec7dee87ce2da03b7a6ca
andreshkivVC <109181911+andreshkivVC@users.noreply.github.com>
2023-04-14 12:18:53 +0300

    зробив бургер, ще потрібно достилізувати

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import { renderCategories } from './js/categories';
 import { changeTheme } from './js/theme-toggle';
 
-const pageEl = document.querySelector('html');
 
+const pageEl = document.querySelector('html');
+const burger = document.getElementById('burger-btn');
+const menu = document.querySelector('.mobile-menu');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  menu.classList.toggle('active');
+});
 // На цей слухач кліків можна вішати всі кліки, що відбуваються на сторінці.
 // Але не перевірено, чи коректно буде працювати на обох сторінках. Якщо ні - поправимо)
 pageEl.addEventListener('click', event => {
