364be38b60c560b05a16d0d4a434ac3545019c35
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-04-14 10:48:46 +0300

    Merge branch 'main' into support-Ukraine

--- src/js/fetchBooks.js
+++ src/js/fetchBooks.js
@@ -3,16 +3,34 @@ import axios from 'axios';
 export class FetchBooks {
   #BASE_URL = 'https://books-backend.p.goit.global/books/';
   constructor() {
-    this.bookId = bookId;
-    this.category = category;
+    this.bookId = 0;
+    this.category = '';
   }
 
-  fetchBookId(bookId) {
+  fetchCategoryList() {
     return axios
-      .get(`${this.#BASE_URL}${bookId}`)
-      .then(response => {
-        response.json();
-      })
+      .get(`${this.#BASE_URL}/category-list`)
+      .then(response => response)
+      .catch(console.error);
+  }
+
+  fetchTopBooks() {
+    return axios
+      .get(`${this.#BASE_URL}/top-books`)
+      .then(response => response)
+      .catch(console.error);
+  }
+  fetchBookId() {
+    return axios
+      .get(`${this.#BASE_URL}${this.bookId}`)
+      .then(response => response)
+      .catch(console.error);
+  }
+
+  fetchCategoryOfBooks() {
+    return axios
+      .get(`${this.#BASE_URL}${this.category}`)
+      .then(response => response)
       .catch(console.error);
   }
 }
