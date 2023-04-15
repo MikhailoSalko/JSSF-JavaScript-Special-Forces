4ad5ef0db2bb43093c50a050f70395beb374de52
ansachuk <ansachuk@icloud.com>
2023-04-15 20:16:54 +0300

    successful import into main.js

--- src/js/categories.js
+++ src/js/categories.js
@@ -1,25 +1,25 @@
-import { FetchBooks } from './fetchBooks';
+import { FetchBooks } from "./fetchBooks";
 
 export { renderCategories, changeCategoryStyle };
-const categoriesEl = document.querySelector('.categories-list');
+const categoriesEl = document.querySelector(".categories-list");
 
 const fetch = new FetchBooks();
 
 function renderCategories() {
   fetch.fetchCategoryList().then(data => {
-    categoriesEl.insertAdjacentHTML('beforeend', renderList(data.data));
+    categoriesEl.insertAdjacentHTML("beforeend", renderList(data.data));
   });
 }
 
 function changeCategoryStyle(event) {
-  const isCategory = event.target.classList.contains('category');
+  const isCategory = event.target.classList.contains("category");
   if (isCategory) {
-    if (event.target.classList.contains('checked-category')) {
+    if (event.target.classList.contains("checked-category")) {
       return;
     } else {
-      const checkedEl = categoriesEl.querySelector('.checked-category');
-      checkedEl.classList.remove('checked-category');
-      event.target.classList.add('checked-category');
+      const checkedEl = categoriesEl.querySelector(".checked-category");
+      checkedEl.classList.remove("checked-category");
+      event.target.classList.add("checked-category");
     }
   }
 }
@@ -27,9 +27,9 @@ function changeCategoryStyle(event) {
 function renderList(categories) {
   return categories
     .map(category => {
-      return renderCategory(category['list_name']);
+      return renderCategory(category["list_name"]);
     })
-    .join('');
+    .join("");
 }
 
 function renderCategory(category) {
