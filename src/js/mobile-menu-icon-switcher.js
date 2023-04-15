15a4885df110c3c79c269993a9252a34161ab39b
ansachuk <ansachuk@icloud.com>
2023-04-15 19:55:33 +0300

    fix function

--- src/js/mobile-menu-icon-switcher.js
+++ src/js/mobile-menu-icon-switcher.js
@@ -1,21 +1,21 @@
 const mobileMenuButtonIconRef = document.querySelector(".mobile-menu__icon");
 const mobileMenuButtonUseRef = mobileMenuButtonIconRef.firstElementChild;
 
-let isOpened = mobileMenuButtonUseRef.getAttribute("is-opened");
-
 const SPRITE_PATH = mobileMenuButtonUseRef.getAttribute("href").split("#")[0];
-
 const MOBILE_MENU_CLOSE_ICON = "burger-menu";
 const MOBILE_MENU_OPEN_ICON = "close";
+
+let isOpened;
 
 mobileMenuButtonIconRef.addEventListener("click", changeMobileMenuIcon);
 
 function changeMobileMenuIcon() {
+  //   debugger;
   isOpened = !isOpened;
 
   currentIcon = isOpened ? MOBILE_MENU_OPEN_ICON : MOBILE_MENU_CLOSE_ICON;
 
-  mobileMenuButtonUseRef.setAttribute("is-opened", isOpened);
-
   mobileMenuButtonUseRef.setAttribute("href", `${SPRITE_PATH}#${currentIcon}`);
+
+  mobileMenuButtonUseRef.setAttribute("is-opened", isOpened);
 }
