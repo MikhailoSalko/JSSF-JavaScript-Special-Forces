d2297efbc3ff60c1ba2d8cf84220a38f5dfccf07
ansachuk <ansachuk@icloud.com>
2023-04-17 13:12:10 +0300

    fix mobile-menu

--- src/main.js
+++ src/main.js
@@ -1,26 +1,22 @@
+import { setTheme, activateThemeToggle } from "./js/theme-toggle";
 import { renderCategories, changeCategoryStyle } from "./js/categories";
+import { currentPageSwitcher } from "./js/current-page-switcher";
+import { changeMobileMenuIcon } from "./js/mobile-menu-icon-switcher";
 import { activateThemeToggle } from "./js/theme-toggle";
 import { changeTheme } from "./js/theme-toggle";
-import { currentPageSwitcher } from "./js/current-page-switcher";
 import "./js/support-ukraine";
 import "./js/authorization-modal";
 
 const pageEl = document.querySelector("html");
 const burger = document.getElementById("burger-btn");
-const menu = document.querySelector(".mobile-menu");
 
 // На сторінці Shop List шукати за класом ".shop-link"
 const currentPage = document.querySelector(".nav__link");
-
 currentPageSwitcher(currentPage);
 
 // Build Categories list
 renderCategories();
 
-burger.addEventListener("click", () => {
-  // burger.classList.toggle("active");
-  menu.classList.toggle("active");
-});
 // На цей слухач кліків можна вішати всі кліки, що відбуваються на сторінці.
 // Але не перевірено, чи коректно буде працювати на обох сторінках. Якщо ні - поправимо)
 pageEl.addEventListener("click", event => {
@@ -31,3 +27,10 @@ pageEl.addEventListener("click", event => {
   //Можна цю функцію помістити у функцію запиту категорій у Інни, але не нахімічити із імпортами
   changeCategoryStyle(event);
 });
+
+// Початкові налаштування теми
+setTheme(pageEl);
+
+//Змінює іконку відкриття/закриття модалки на мобілкі. Пізніше привʼяжемо ще відкриття і закриття самої модалки
+const mobileMenuButtonIconRef = document.querySelector(".mobile-menu__icon");
+mobileMenuButtonIconRef.addEventListener("click", changeMobileMenuIcon);
