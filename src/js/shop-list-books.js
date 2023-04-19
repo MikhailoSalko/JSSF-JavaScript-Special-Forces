b5f12a8805529aaba8dbeb0612d404f09f8df08a
PyotrKlim <113846380+PyotrKlim@users.noreply.github.com>
2023-04-19 03:02:39 +0300

    feature/pagination
    первичное сохранение

--- src/js/shop-list-books.js
+++ src/js/shop-list-books.js
@@ -182,6 +182,183 @@ const books = [
     ],
     __v: 0,
   },
+  {
+    _id: '642fd89ac8cf5ee957f12361',
+    list_name: 'Middle Grade Paperback Monthly',
+    date: '2023-04-07T08:46:57.000Z',
+    age_group: '',
+    amazon_product_url:
+      'https://www.amazon.com/Wish-Barbara-OConnor/dp/1250144051?tag=NYTBSREV-20',
+    article_chapter_link: '',
+    author: "Barbara O'Connor",
+    book_image:
+      'https://storage.googleapis.com/du-prd/books/images/9781250144058.jpg',
+    book_image_width: 330,
+    book_image_height: 485,
+    book_review_link: '',
+    book_uri: 'nyt://book/46604242-8624-57d1-bdd4-424c21cde273',
+    contributor: "by Barbara O'Connor",
+    contributor_note: '',
+    created_date: '2023-04-05 23:10:17',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada, nibh a bibendum varius, enim tortor dictum felis, vel tristique tellus justo ac sem. Duis quis magna vel sapien euismod vestibulum.',
+    first_chapter_link: '',
+    price: '0.00',
+    primary_isbn10: '1250144051',
+    primary_isbn13: '9781250144058',
+    publisher: 'Square Fish',
+    rank: 1,
+    rank_last_week: 0,
+    sunday_review_link: '',
+    title: 'WISH',
+    updated_date: '2023-04-05 23:10:17',
+    weeks_on_list: 0,
+    buy_links: [
+      {
+        name: 'Amazon',
+        url: 'https://www.amazon.com/Wish-Barbara-OConnor/dp/1250144051?tag=NYTBSREV-20',
+      },
+      {
+        name: 'Apple Books',
+        url: 'https://goto.applebooks.apple/9781250144058?at=10lIEQ',
+      },
+      {
+        name: 'Barnes and Noble',
+        url: 'https://www.anrdoezrs.net/click-7990613-11819508?url=https%3A%2F%2Fwww.barnesandnoble.com%2Fw%2F%3Fean%3D9781250144058',
+      },
+      {
+        name: 'Books-A-Million',
+        url: 'https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fp%252FWISH%252FBarbara%252BO%252527Connor%252F9781250144058&url2=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fsearch%253Fquery%253DWISH%252BBarbara%252BO%252527Connor',
+      },
+      {
+        name: 'Bookshop',
+        url: 'https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fbookshop.org%2Fa%2F3546%2F9781250144058&url2=https%3A%2F%2Fbookshop.org%2Fbooks%3Faffiliate%3D3546%26keywords%3DWISH',
+      },
+      {
+        name: 'IndieBound',
+        url: 'https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.indiebound.org%2Fbook%2F9781250144058%3Faff%3DNYT&url2=https%3A%2F%2Fwww.indiebound.org%2Fsearch%2Fbook%3Fkeys%3DWISH%2BBarbara%2BO%2527Connor%26aff%3DNYT',
+      },
+    ],
+    __v: 0,
+  },
+  {
+    _id: '642fd89ac8cf5ee957f122da',
+    list_name: 'Paperback Nonfiction',
+    date: '2023-04-07T08:46:57.000Z',
+    age_group: '',
+    amazon_product_url:
+      'http://www.amazon.com/The-Body-Keeps-Score-Healing/dp/0670785938?tag=NYTBSREV-20',
+    article_chapter_link: '',
+    author: 'Bessel van der Kolk',
+    book_image:
+      'https://storage.googleapis.com/du-prd/books/images/9780670785933.jpg',
+    book_image_width: 128,
+    book_image_height: 193,
+    book_review_link:
+      'https://www.nytimes.com/2018/10/18/books/review/how-to-rewire-your-traumatized-brain.html',
+    book_uri: 'nyt://book/e3e33e9d-0e67-5fec-b0d2-2ecddc38ce0e',
+    contributor: 'by Bessel van der Kolk',
+    contributor_note: '',
+    created_date: '2023-04-05 22:05:27',
+    description:
+      'How trauma affects the body and mind, and innovative treatments for recovery.',
+    first_chapter_link: '',
+    price: '0.00',
+    primary_isbn10: '0143127748',
+    primary_isbn13: '9780143127741',
+    publisher: 'Penguin',
+    rank: 1,
+    rank_last_week: 1,
+    sunday_review_link: '',
+    title: 'THE BODY KEEPS THE SCORE',
+    updated_date: '2023-04-05 22:10:17',
+    weeks_on_list: 232,
+    buy_links: [
+      {
+        name: 'Amazon',
+        url: 'http://www.amazon.com/The-Body-Keeps-Score-Healing/dp/0670785938?tag=NYTBSREV-20',
+      },
+      {
+        name: 'Apple Books',
+        url: 'https://goto.applebooks.apple/9780143127741?at=10lIEQ',
+      },
+      {
+        name: 'Barnes and Noble',
+        url: 'https://www.anrdoezrs.net/click-7990613-11819508?url=https%3A%2F%2Fwww.barnesandnoble.com%2Fw%2F%3Fean%3D9780143127741',
+      },
+      {
+        name: 'Books-A-Million',
+        url: 'https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fp%252FTHE%252BBODY%252BKEEPS%252BTHE%252BSCORE%252FBessel%252Bvan%252Bder%252BKolk%252F9780143127741&url2=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fsearch%253Fquery%253DTHE%252BBODY%252BKEEPS%252BTHE%252BSCORE%252BBessel%252Bvan%252Bder%252BKolk',
+      },
+      {
+        name: 'Bookshop',
+        url: 'https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fbookshop.org%2Fa%2F3546%2F9780143127741&url2=https%3A%2F%2Fbookshop.org%2Fbooks%3Faffiliate%3D3546%26keywords%3DTHE%2BBODY%2BKEEPS%2BTHE%2BSCORE',
+      },
+      {
+        name: 'IndieBound',
+        url: 'https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.indiebound.org%2Fbook%2F9780143127741%3Faff%3DNYT&url2=https%3A%2F%2Fwww.indiebound.org%2Fsearch%2Fbook%3Fkeys%3DTHE%2BBODY%2BKEEPS%2BTHE%2BSCORE%2BBessel%2Bvan%2Bder%2BKolk%26aff%3DNYT',
+      },
+    ],
+    __v: 0,
+  },
+  {
+    _id: '642fd89ac8cf5ee957f122db',
+    list_name: 'Paperback Nonfiction',
+    date: '2023-04-07T08:46:57.000Z',
+    age_group: '',
+    amazon_product_url: 'https://www.amazon.com/dp/0525657746?tag=NYTBSREV-20',
+    article_chapter_link: '',
+    author: 'Michelle Zauner',
+    book_image:
+      'https://storage.googleapis.com/du-prd/books/images/9780525657743.jpg',
+    book_image_width: 338,
+    book_image_height: 500,
+    book_review_link: '',
+    book_uri: 'nyt://book/520cb070-10ce-5d43-9b0c-e77f57d84a8d',
+    contributor: 'by Michelle Zauner',
+    contributor_note: '',
+    created_date: '2023-04-05 22:05:27',
+    description:
+      'The daughter of a Korean mother and Jewish American father, and leader of the indie rock project Japanese Breakfast, describes creating her own identity after losing her mother to cancer.',
+    first_chapter_link: '',
+    price: '0.00',
+    primary_isbn10: '1984898957',
+    primary_isbn13: '9781984898951',
+    publisher: 'Vintage',
+    rank: 2,
+    rank_last_week: 0,
+    sunday_review_link: '',
+    title: 'CRYING IN H MART',
+    updated_date: '2023-04-05 22:10:17',
+    weeks_on_list: 1,
+    buy_links: [
+      {
+        name: 'Amazon',
+        url: 'https://www.amazon.com/dp/0525657746?tag=NYTBSREV-20',
+      },
+      {
+        name: 'Apple Books',
+        url: 'https://goto.applebooks.apple/9781984898951?at=10lIEQ',
+      },
+      {
+        name: 'Barnes and Noble',
+        url: 'https://www.anrdoezrs.net/click-7990613-11819508?url=https%3A%2F%2Fwww.barnesandnoble.com%2Fw%2F%3Fean%3D9781984898951',
+      },
+      {
+        name: 'Books-A-Million',
+        url: 'https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fp%252FCRYING%252BIN%252BH%252BMART%252FMichelle%252BZauner%252F9781984898951&url2=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fsearch%253Fquery%253DCRYING%252BIN%252BH%252BMART%252BMichelle%252BZauner',
+      },
+      {
+        name: 'Bookshop',
+        url: 'https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fbookshop.org%2Fa%2F3546%2F9781984898951&url2=https%3A%2F%2Fbookshop.org%2Fbooks%3Faffiliate%3D3546%26keywords%3DCRYING%2BIN%2BH%2BMART',
+      },
+      {
+        name: 'IndieBound',
+        url: 'https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.indiebound.org%2Fbook%2F9781984898951%3Faff%3DNYT&url2=https%3A%2F%2Fwww.indiebound.org%2Fsearch%2Fbook%3Fkeys%3DCRYING%2BIN%2BH%2BMART%2BMichelle%2BZauner%26aff%3DNYT',
+      },
+    ],
+    __v: 0,
+  },
 ];
 
 localStorage.setItem('books', JSON.stringify(books));
@@ -328,3 +505,114 @@ bookListEl.addEventListener('click', event => {
     deleteBook(bookId);
   }
 });
+
+// \/------- pagination ------\/
+const sectionShoplist = document.querySelector(".shoplist");
+const heading = document.createElement("div");
+heading.className = "pagination-container";
+sectionShoplist.append(heading)
+
+const pageSize = 2;
+      let activPage = 1;
+      let totalPages = Math.ceil(booksInShop.length / pageSize);
+      console.log(totalPages)
+
+      const displayPagination = () => {
+        const containerPagination = document.querySelector(
+          ".pagination-container"
+        );
+        containerPagination.innerHTML = "";
+
+        const firstPage = document.createElement("button");
+        firstPage.className = "button-pagination"
+        firstPage.innerHTML = "&#171;";
+        firstPage.disabled = activPage === 1;
+        firstPage.addEventListener("click", () => {
+          activPage = 1;
+          displayData(0);
+          displayPagination();
+        });
+        containerPagination.appendChild(firstPage);
+
+        const prevPage = document.createElement("button");
+        prevPage.className = "button-pagination"
+        prevPage.innerHTML = "&#8249;";
+        prevPage.disabled = activPage === 1;
+        prevPage.addEventListener("click", () => {
+          activPage--;
+          const startIndex = (activPage - 1) * pageSize;
+          displayData(startIndex);
+          displayPagination();
+        });
+        containerPagination.appendChild(prevPage);
+
+        let startPage =
+          activPage <= 2
+            ? 1
+            : activPage >= totalPages - 1
+            ? totalPages - 2
+            : activPage - 1;
+
+        const dotsStart = startPage > 1;
+        if (dotsStart) {
+          const dots = document.createElement("button");
+          dots.className = "button-pagination"
+          dots.innerHTML = "&#8230;";
+          containerPagination.appendChild(dots);
+        }
+
+        for (let i = startPage; i < startPage + 3 && i <= totalPages; i++) {
+          const page = document.createElement("button");
+          page.className = "button-pagination"
+          page.innerHTML = i;
+          page.disabled = i === activPage;
+          page.classList.toggle("active", i === activPage);
+          page.addEventListener("click", () => {
+            activPage = i;
+            const startIndex = (activPage - 1) * pageSize;
+            displayData(startIndex);
+            displayPagination();
+          });
+          containerPagination.appendChild(page);
+        }
+
+        const dotsEnd = startPage + 2 < totalPages;
+        if (dotsEnd) {
+          const dots = document.createElement("button");
+          dots.className = "button-pagination"
+          const dotsEnd = startPage + 2 < totalPages;
+          if (dotsEnd) {
+            const dots = document.createElement("button");
+            dots.className = "button-pagination"
+            dots.innerHTML = "&#8230;";
+            containerPagination.appendChild(dots);
+          }
+
+          const nextPage = document.createElement("button");
+          dots.nextPage = "button-pagination"
+          nextPage.innerHTML = "&#8250;";
+          nextPage.disabled = activPage === totalPages;
+          nextPage.addEventListener("click", () => {
+            activPage++;
+            const startIndex = (activPage - 1) * pageSize;
+            displayData(startIndex);
+            displayPagination();
+          });
+          containerPagination.appendChild(nextPage);
+
+          const lastPage = document.createElement("button");
+          dots.lastPage = "button-pagination"
+          lastPage.innerHTML = "&#187;";
+          lastPage.disabled = activPage === totalPages;
+          lastPage.addEventListener("click", () => {
+            activPage = totalPages;
+            const startIndex = (totalPages - 1) * pageSize;
+            displayData(startIndex);
+            displayPagination();
+          });
+          containerPagination.appendChild(lastPage);
+        }
+      };
+
+          displayPagination();
+          // /\------- pagination ------/\
