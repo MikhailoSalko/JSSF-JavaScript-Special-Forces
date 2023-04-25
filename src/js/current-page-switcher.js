8ae50053e2d7311693e8f130f1f024b12bd157ac
Oleksandr Moskaliuk <olekspm@gmail.com>
2023-04-25 09:01:53 +0200

    Fixed current page switcher

--- src/js/current-page-switcher.js
+++ src/js/current-page-switcher.js
@@ -1,5 +1,28 @@
-function currentPageSwitcher(currentPage) {
-  currentPage.classList.add("current-page");
+import { Notify } from 'notiflix';
+export { setActivePage, blockReloadIfCurrent };
+
+const navClass = 'nav__link';
+const currentPageClass = 'current-page';
+
+const navLinkEls = document.querySelectorAll(`.${navClass}`);
+
+function setActivePage() {
+  navLinkEls.forEach(el => {
+    if (window.location.pathname === el.getAttribute('href')) {
+      el.classList.add(currentPageClass);
+    } else {
+      if (el.classList.contains(currentPageClass)) {
+        el.classList.remove(currentPageClass);
+      }
+    }
+  });
 }
 
-export { currentPageSwitcher };
+function blockReloadIfCurrent(event) {
+  if (event.target.classList.contains(currentPageClass)) {
+    event.preventDefault();
+    Notify.info(
+      `This is the ${event.target.textContent.toLowerCase()} page already`
+    );
+  }
+}
